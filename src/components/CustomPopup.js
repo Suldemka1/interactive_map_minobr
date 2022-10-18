7e663b08ddbedf9f61dd41e7a4815b613826696f
Suldemka1 <suldemka1@gmail.com>
2022-10-18 12:26:46 +0700

    popup modified #1

--- src/components/CustomPopup.js
+++ src/components/CustomPopup.js
@@ -5,79 +5,112 @@ import L from "leaflet";
 const CustomPopup = (params) => {
     const customIcon = new L.Icon({
         iconUrl: require('../location.png').default,
-        iconSize: new L.Point(40, 47),
+        iconSize: new L.Point(50, 50),
     })
 
     return (
         <Marker key={params.data.id} position={[params.data.k1, params.data.k2]} icon={customIcon}>
             <Popup>
-                <div>
-                    {params.data.name}
-                </div>
-                <div>
-                    {params.data.trosta === true && <p>ТОЧКА РОСТА</p>}
-                    {params.data['cos'] === true && <p>ЦОС</p>}
-                    {params.data.itcube === true && <p>IT-cube</p>}
-                    {params.data.kvant === true && <p>Кванториум</p>}
-                    <div>
-                        <p>В это учебном заведении есть:</p>
-                        {
-                            params.data.mastery != null ?
-                                params.data.mastery.map((item) => <p>{item.name}</p>)
-                                :
-                                <p>asd</p>
-                        }
+                <div className="card">
+                    <div className="header">
+                        <div className="org-name">
+                            {params.data.name}
+                        </div>
                     </div>
-                </div>
-                    {
-                        params.data.hot_meals
-                            ?
-                            <details>
-                                <summary>Информация об организации горячего питания</summary>
-                                <div>
-                                    {params.data.hot_meals}
-                                </div>
-                            </details>
-                            :
-                            <div>Информация об организации горячего питания отсутсвует</div>
-                    }
+                    <div className="body">
+                        <div className="features">
 
-                    <details>
-                        <summary>Контактные данные</summary>
-                        <div>
-                            <div>
-                                {params.data.address.code} {params.data.address.region} {params.data.address.street} д.
-                                {params.data.address.number}
-                            </div>
-                            <div>
-                                <p>Адрес электронной почты: </p>
-                                <p>{params.data.contacts.email}</p>
-                            </div>
+                            {params.data.trosta === true &&
+                                <div className="feature"><img src="/trosta.png"/>ТОЧКА РОСТА</div>}
+                            {params.data['cos'] === true && <div className="feature"><img src="/cos.png"/>ЦОС</div>}
+                            {params.data.itcube === true &&
+                                <div className="feature"><img src="/itcube.png"/>IT-cube</div>}
+                            {params.data.kvant === true &&
+                                <div className="feature"><img src="/kvant.png"/>Кванториум</div>}
 
-                            <div className="phones">
+                            <div>
                                 {
-                                    params.data.contacts.phones.map((item) => {
-                                        if (item.phone.length > 3)
-                                            return <div key={item.phone}>
-                                                <p>Телефон:</p>
-                                                <p>{item.phone}</p>
+                                    params.data.mastery != null
+                                        ?
+                                        <details>
+                                            <summary>В это учебном заведении есть мастерские:</summary>
+                                            <div>
+                                                {
+                                                    params.data.mastery.map((item) => <p>{item.name}</p>)
+                                                }
                                             </div>
-                                    })
-                                }
-                            </div>
-
-                            <div>
-                                {
-                                    params.data.web_site.length > 6 && <a href={params.data.web_site}>
-                                        Перейти на сайт
-                                    </a>
+                                        </details>
+                                        :
+                                        null
                                 }
                             </div>
                         </div>
-                    </details>
+
+                        {
+                            params.data.hot_meals
+                                ?
+                                <details>
+                                    <summary>Информация об организации горячего питания</summary>
+                                    <div className="hot-meals">
+                                        {params.data.hot_meals}
+                                    </div>
+                                </details>
+                                :
+                                null
+                        }
+                    </div>
+
+                    <div className="footer">
+                        <details>
+                            <summary>Контактные данные</summary>
+                            <div className="contacts">
+                                <div className="address">
+                                    {params.data.address.code} {params.data.address.region} {params.data.address.street} д.
+                                    {params.data.address.number}
+                                </div>
+                                {
+                                    params.data.contacts.email.length > 1
+                                        ?
+                                        <div className="email">
+                                            <p>Адрес электронной почты: </p>
+                                            <p>{params.data.contacts?.email}</p>
+                                        </div>
+                                        :
+                                        null
+                                }
+                                <div className="phones">
+                                    {
+                                        params.data.contacts.phones.length > 1
+                                            ?
+                                            params.data.contacts.phones.map((item) => {
+                                                if (item.phone.length > 3)
+                                                    return (
+                                                        <div className="phone" key={item.phone}>
+                                                            <p>Телефон:</p>
+                                                            <p>{item.phone}</p>
+                                                        </div>
+                                                    )
+                                            })
+                                            :
+                                            null
+                                    }
+                                </div>
+                                <div className="website">
+                                    {
+                                        params.data.web_site.length > 6 && <a href={params.data.web_site}>
+                                            Перейти на сайт
+                                        </a>
+                                    }
+                                </div>
+                            </div>
+                        </details>
+                    </div>
+                </div>
+
+
             </Popup>
         </Marker>
-);
+    );
 };
 
 export default CustomPopup;
